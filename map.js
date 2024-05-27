7fb1d012812b9da2a9c06958dbbb43131c6420d6
Nitin Jangir <115369751+nitinjangir013@users.noreply.github.com>
2024-05-27 19:22:16 +0530

    API KEY UPDATE

--- map.js
+++ map.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     // Your Google Maps API key
-    var apiKey = 'AIzaSyAILGVlt-SOiL381JT3TQ9dxxoNIUuxrV8';
+    var apiKey = 'YOUR_GOOGLE_MAP_API_KEY'; // Replace Your Google Maps API key
 
     // The address you want to show on the map
-    var address = '1600 Amphitheatre Parkway, Mountain View, CA';
+    var address = '1600 Amphitheatre Parkway, Mountain View, CA'; // Replace
     
     var encodedAddress = encodeURIComponent(address);
 
